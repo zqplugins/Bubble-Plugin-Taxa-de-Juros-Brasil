88ac6e4df64e35326842427efbe567948050427b
zqplugins <137153217+zqplugins@users.noreply.github.com>
2023-10-30 17:02:04 +0200

    Automatic commit from Bubble

---
+++ actions/AAC-850m6/server.js
@@ -0,0 +1,15 @@
+async function(properties, context) {
+    const selic = require('selic');
+    try {
+        const response = await selic.getRatesObject(); // { selic: 7.75, cdi: 7.65, ipca: 9.32 }
+        return {
+            selic: response.selic,
+            cdi: response.cdi,
+            ipca: response.ipca
+        };
+    } catch (err) {
+        console.error(err); // Log the error for debugging.
+        // Depending on your needs, you can throw the error or return a default/error response.
+        throw err;
+    }
+}
